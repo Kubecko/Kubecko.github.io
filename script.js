36c786ec2438cdc4af21a8afd9cd05fd39222b2f
Kubecko <MKubecko@gmail.com>
2018-10-11 00:46:26 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8888/tasks_frontend/';
+  const apiRoot = 'https://polar-dusk-90431.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://polar-dusk-90431.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
